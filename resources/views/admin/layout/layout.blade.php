0878186c0eaa02e91d2ecac944efea856ec62794
Arman Hossain <armanhossaincse7@gmail.com>
2024-02-07 19:35:34 +0600

    create AdminController and the route with the currect path

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -31,12 +31,12 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{url('admin/images/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
   </div>
 
     @include('admin.layout.header')
     @include('admin.layout.sidebar')
-    @yield
+    @yield('content')
 
     @include('admin.layout.footer')
   <!-- Control Sidebar -->
