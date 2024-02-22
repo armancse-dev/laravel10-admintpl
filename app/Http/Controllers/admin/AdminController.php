61316de8676b46da67b3b26e5de37bb6d9af59be
Arman Hossain <armanhossaincse7@gmail.com>
2024-02-22 18:48:07 +0600

    login logout form validation

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
+use Validator;
 class AdminController extends Controller
 {
     public function dashboard(){
@@ -15,6 +16,20 @@ class AdminController extends Controller
         if($request->isMethod('post')){
             $data = $request->all();
             // echo "<pre>"; print_r($data); die;
+
+            $rules = [
+                'email' => 'required|email|max:255',
+                'password' => 'required|max:30',
+            ];
+
+            $customMessages = [
+                'email.required' => "Email is required",
+                'email.email' => "Valid Email is required",
+                'email.required' => "Password is required",
+            ];
+
+            $this->validate($request,$rules,$customMessages);
+
             if(Auth::guard('admin')->attempt(['email' => $data['email'], 'password' => $data['password']])){
                 return redirect('admin/dashboard');
             }else{
